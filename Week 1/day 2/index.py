68347b46956a53f5aa7c293837b2004dbd4abadf
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-07-25 22:19:10 -0400

    final commit for day 2, incomplete

---
+++ Week 1/day 2/index.py
@@ -0,0 +1,17 @@
+print(len('hello'))
+#print(len(123)) # will throw type error
+print(len(str(123)))
+print('Hello'[0])
+print('Hello'[len('hello')-1])
+
+# integer
+print('1' + '1')
+# print(1 + '1') #throws error, no type coersion like JS
+print(1 + 1)
+print(123_123_123) # can use _ in Int or Float to make number more readable
+
+# float
+print(1.1 + 1)
+
+# boolean
+print(True)
