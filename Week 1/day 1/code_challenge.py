5b22dd8b14644f32f82ba54b2633d3bacaf60377
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-07-25 22:08:41 -0400

    complete day 1

---
+++ Week 1/day 1/code_challenge.py
@@ -0,0 +1,23 @@
+# print('Day 1 - Python Print Functions')
+# print('The function is declared like this:')
+# print("print('what to print')")
+# print('\n')
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the '+' sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+# print('\n')
+
+# print(len(input('What is your name? ')))
+# print('\n')
+
+a = input('a: ')
+b = input('b: ')
+
+temp = a
+a = b
+b = temp
+
+print('a: ' + a)
+print('b: ' + b)
---
+++ Week 1/day 1/index.py
@@ -0,0 +1,14 @@
+print('Hello World\nHello World Again')
+
+city = input('What is the name of the city you grew up in?\n')
+pet = input('What is the name of your first pet?\n')
+
+band_name = city + ' ' + pet
+print('Your band name is: ' + band_name)
+
+# print('Hello ' + input('What is your name? ') + '!')
+
+# name = input('What is your name? ')
+# name_length = str(len(name))
+# print('Your name has ' + name_length + ' letters!')
+
