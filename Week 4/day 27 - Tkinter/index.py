5618120ed2754fb3c29b1cc814796fbaf1d455a4
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-08-19 22:57:57 -0400

    part of day 27

---
+++ Week 4/day 27 - Tkinter/index.py
@@ -0,0 +1,20 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title('First Gui program')
+window.minsize(width = 800, height = 600)
+
+# label
+my_label = tkinter.Label(text='I am a label', font=('Arial', 24, 'bold'))
+my_label.pack(side = 'left')
+
+# unlimited args (similar to ... in js)
+def add(*args):
+  print(sum(args))
+
+add(1, 2, 3)
+
+
+# keeps window open
+window.mainloop()
