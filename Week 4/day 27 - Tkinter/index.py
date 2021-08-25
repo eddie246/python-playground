10c4a43c0a4974c1beab52c8b6533ffd93b01520
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-08-24 23:53:04 -0400

    part of day 27

--- Week 4/day 27 - Tkinter/index.py
+++ Week 4/day 27 - Tkinter/index.py
@@ -5,26 +5,28 @@ window = tkinter.Tk()
 
 window.title('First Gui program')
 window.minsize(width = 800, height = 600)
+window.config(padx=100, pady=100)
 
 # label
 my_label = tkinter.Label(text='I am a label', font=('Arial', 24, 'bold'))
-my_label.grid( 0,0)
+my_label.grid(column=0, row=0)
 
 def handle_click():
   my_label['text'] = input.get()
 
 button = tkinter.Button(text="Click Me", command=handle_click)
-button.grid(1,1)
+button.grid(column=1, row=1)
 
 new_button = tkinter.Button(text="New One")
-new_button.grid(0, 2)
+new_button.grid(column=0, row=2)
 
 # entry
 input = tkinter.Entry(width=20)
 input.insert(END, 'Some default text')
-input.grid(3, 2)
+input.grid(column=3, row=2)
 
 
 
 # keeps window open
 window.mainloop()
+# wdaw
